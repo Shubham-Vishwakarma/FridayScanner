4291394249ac9308f45c9426f90f49a7b23bb5aa
abhishek97rai <abhishekrai.apj@gmail.com>
2017-09-09 17:57:47 +0530

    My First Commit

--- app/src/main/java/com/example/oslab/friscanner/MainActivity.java
+++ app/src/main/java/com/example/oslab/friscanner/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.oslab.friscanner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8",
+            "Img9",
+            "Img10",
+            "Img11",
+            "Img12",
+            "Img13",
+            "Img14",
+            "Img15",
+            "Img16",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.businesscard,
+            R.drawable.businesscard1,
+            R.drawable.businesscard2,
+            R.drawable.circular,
+            R.drawable.circular1,
+            R.drawable.letter,
+            R.drawable.news,
+            R.drawable.news1,
+            R.drawable.news2,
+            R.drawable.notice,
+            R.drawable.notice1,
+            R.drawable.notice3,
+            R.drawable.storybook,
+            R.drawable.storybook1,
+            R.drawable.storybook2,
+            R.drawable.storybook3,
+    };
+
+    private ArrayList<CreateList> prepareData(){
+
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
     }
 }
